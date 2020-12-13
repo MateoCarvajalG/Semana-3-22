77e507ca49000973918098780a6610f5bb4d1876
Mateo carvajal garcia <mcarvajalg10@gmail.com>
2020-12-12 22:12:26 -0500

    ajuste del backend

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const db = require('./models');
 const app = express()
 const bodyParser = require('body-parser');
 const apiRouter = require('./routes');
+const cors = require('cors');
+app.use(cors())
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
